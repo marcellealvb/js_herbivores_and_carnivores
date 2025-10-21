dcd2815e9ca7dbc23ca399a50bf7dd77493de09e
MarcelLeal <marcel@pgentulhos.com.br>
2025-10-21 17:58:59 -0300

    development of Herbivores and Carnivores

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,16 +1,82 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+
+  constructor(Health = 100, name) {
+    this.Health = Health;
+    this.name = name;
+
+  }
+
+  static updateAlivelist() {
+    Animal.alive = Animal.alive.filter(animal => animal.health > 0);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+
+  constructor(name) {
+    super(100, name)
+    this.hidden = false
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+
+  constructor(name) {
+    super(100, name);
+  }
+
+  bite(target) {
+    if (!(target instanceof Herbivore)) {
+      return;
+    }
+    if (target.hidden) {
+      return;
+    }
+
+    target.health -= 50;
+    Animal.updateAlivelist();
+  }
 }
+
+const deer = new Herbivore('Bembi');
+const panther = new Carnivore('Bagira');
+const lion = new Carnivore('King');
+const rabbit = new Herbivore('Max');
+
+Animal.alive [
+  {name: 'Bembi', health: 100, hidden: false},
+  {name: 'Bagira', health: 100},
+  {name: 'King', health: 100},
+  {name: 'Max', health: 100, hidden: false}
+];
+
+lion.bite(deer);
+panther.bite(lion);
+
+Animal.alive [
+  {name: 'Bembi', health: 50},
+  {name: 'Bagira', health: 100},
+  {name: 'King', health: 100},
+  {name: 'Max', health: 100}
+];
+
+panther.bite(deer);
+rabbit.hide();
+panther.bite(rabbit);
+
+Animal.alive [
+  {name: 'Bagira', health: 100},
+  {name: 'King', health: 100},
+  {name: 'Max', health: 100, hidden: true}
+];
 
 module.exports = {
   Animal,
